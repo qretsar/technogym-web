eae5f48cdb1551d310fb1df8493066dce5cd70fe
Tin-Mijatovic <tinbe.digital@gmail.com>
2021-11-28 20:51:46 +0100

    rasdfasdfasdf

--- src/App.js
+++ src/App.js
@@ -45,9 +45,10 @@ function App() {
     console.log("Povlacim iz baze", new Date());
   }, [setMembers, jelena]);
   // [form, status, filteredMembers]);
-  useEffect(() => {
-    filterHandler();
-  }, [setMembers, members, search, status, jelena]);
+  // useEffect(() => {
+  //   filterHandler();
+
+  // }, [setMembers, members, search, status, jelena]);
   //FUNCTIONS
   const filterHandler = () => {
     let activeMembers = members.map((member) => {
